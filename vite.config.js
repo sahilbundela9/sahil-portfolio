883220d7984e15fa56be632fd235cf6a48b50287
sahilbundela9 <bundelasahil634@gmail.com>
2026-03-18 22:26:50 +0530

    fINAL fIC

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',           // ← THIS fixes white screen on Netlify
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
